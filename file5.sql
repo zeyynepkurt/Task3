1bd17b6a40d84c6ab4527611f8787220f51ad8f8
Zeynep Kurt <zeynepkurt825@marun.edu.tr>
2025-08-06 13:35:52 +0300

    file5 updated

--- file5.sql
+++ file5.sql
@@ -9,21 +9,27 @@ departments_with_employees AS (
 departments_without_employees AS (
   SELECT ad.department_name
   FROM all_departments ad
-    LEFT JOIN departments_with_employees dwe ON ad.department_id = dwe.department_id
+  LEFT JOIN departments_with_employees dwe
+    ON ad.department_id = dwe.department_id
   WHERE dwe.department_id IS NULL
 ),
-char_expanded AS (
-  SELECT department_name, LEVEL AS pos,
-    SUBSTR(department_name, LENGTH(department_name) - LEVEL + 1, 1) AS chr
+char_parts AS (
+  SELECT 
+    department_name,
+    SUBSTR(department_name, LENGTH(department_name) - level + 1, 1) AS chr,
+    level AS position
   FROM departments_without_employees
-  CONNECT BY LEVEL <= LENGTH(department_name)
+  CONNECT BY level <= LENGTH(department_name)
     AND PRIOR department_name = department_name
     AND PRIOR DBMS_RANDOM.VALUE IS NOT NULL
 ),
 reversed_names AS (
-  SELECT department_name,
-    LISTAGG(chr, '') WITHIN GROUP (ORDER BY pos) AS reversed_name
-  FROM char_expanded
+  SELECT 
+    department_name,
+    LISTAGG(chr, '') WITHIN GROUP (ORDER BY position) AS reversed_name
+  FROM char_parts
   GROUP BY department_name
 )
 SELECT * FROM reversed_names;
+
+
