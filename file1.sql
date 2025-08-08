7cee45fd29ed058638eb44fd437a3348bb0f733c
Zeynep Kurt <109173044+zeyynepkurt@users.noreply.github.com>
2025-08-08 15:53:15 +0300

    file1 updated

--- file1.sql
+++ file1.sql
@@ -1,18 +1,18 @@
-WITH salary_ranked AS (
-  SELECT 
+WITH ranked_employees AS (
+  SELECT
     employee_id,
     first_name,
     last_name,
     department_id,
     salary,
-    MAX(salary) OVER (PARTITION BY department_id) AS max_dept_salary
+    RANK() OVER (PARTITION BY department_id ORDER BY salary DESC) AS salary_rank
   FROM HR.EMPLOYEES
 )
-SELECT 
+SELECT
   employee_id,
   first_name,
   last_name,
   department_id,
   salary
-FROM salary_ranked
-WHERE salary = max_dept_salary;
+FROM ranked_employees
+WHERE salary_rank = 1;
